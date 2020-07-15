0341384cd19e445c2a4a5d85d269332e83677723
dvru <damvaru4@gmail.com>
2020-07-15 17:30:44 -0500

    React concepts

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+    }
+}

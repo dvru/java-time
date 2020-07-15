603ffca805f2b1a3a94a94de5a6dbad6e0ccc877
dvru <damvaru4@gmail.com>
2020-07-15 18:42:35 -0500

    primitive datatypes

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -34,5 +34,11 @@ public class Main {
         System.out.println(bigLongLiteralValue);
 
         short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte)(myMinByteValue / 2);
+
+        short myNewShortValue = (short)(myMinShortValue / 2);
     }
 }

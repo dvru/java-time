dead17fe30defcb83473a84b00aba56af93dc641
dvru <damvaru4@gmail.com>
2020-07-15 18:54:10 -0500

    primitive datatypes challange

---
+++ challenge/src/challange.java
@@ -0,0 +1,15 @@
+public class challange {
+    public static void main(String[] args) {
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L *
+                (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+    }
+}
+
+//create a byte, short and int variable with valid numbers,
+//create a variable of type long, make it equal to 5000 plus 10 times the sum of byte plus the short plus the integer values

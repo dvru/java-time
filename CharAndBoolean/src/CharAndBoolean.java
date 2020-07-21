97b4449edf662f5117f6b94c94cec552cd106e96
dvru <damvaru4@gmail.com>
2020-07-21 12:59:54 -0500

    char and boolean

---
+++ CharAndBoolean/src/CharAndBoolean.java
@@ -0,0 +1,20 @@
+public class CharAndBoolean {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+    }
+
+}

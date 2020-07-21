89fdb5df01390b35740c8b8dfa2ae6e0c6b7b107
dvru <damvaru4@gmail.com>
2020-07-21 12:07:01 -0500

    float and double

---
+++ FloatAndDouble/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum value = " + myMinDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;  // float myFloatValue = (float)5.25;
+        double myDoubleValue = 5.25d;
+
+    }
+    
+}

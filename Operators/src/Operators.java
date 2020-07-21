1da5993e4eba88844af5f1a8ab2388832b60318f
dvru <damvaru4@gmail.com>
2020-07-21 14:08:59 -0500

    operators

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -43,6 +43,12 @@ public class Operators {
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (!isAlien == false){
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
     }
 
 }

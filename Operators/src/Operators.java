a50e79aca6520005f38dca76130e313b5d5765cf
dvru <damvaru4@gmail.com>
2020-07-21 14:17:27 -0500

    operators

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -44,10 +44,20 @@ public class Operators {
         System.out.println("10 - 2 = " + result);
 
         boolean isAlien = false;
-        if (!isAlien == false){
+        if (isAlien == false){
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        } System.out.println("dumgai");
 
     }
 

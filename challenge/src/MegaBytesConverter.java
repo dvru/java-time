34c945588bfbbfbc8e6e8ac000f5fe08be91dc48
dvru <damvaru4@gmail.com>
2020-07-22 15:52:01 -0500

    MegaBytesConverter

---
+++ challenge/src/MegaBytesConverter.java
@@ -0,0 +1,20 @@
+public class MegaBytesConverter {
+
+    public static void main(String[] args) {
+
+        printMegaBytesAndKiloBytes(2500);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megabytes = (kiloBytes / 1024);
+            int kiloRemainder = (kiloBytes % 1024);
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + kiloRemainder + " KB");
+        }
+    }
+}
+
+
+

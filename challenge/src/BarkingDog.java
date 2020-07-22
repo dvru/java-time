eb1ff161e2f7700f4e5717726bff66c556310842
dvru <damvaru4@gmail.com>
2020-07-22 17:25:14 -0500

    BarkingDog

---
+++ challenge/src/BarkingDog.java
@@ -0,0 +1,25 @@
+public class BarkingDog {
+
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp(true,23));
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (!barking || hourOfDay < 0 || hourOfDay > 23){
+            return false;
+        } else if (hourOfDay < 8) {
+            return true;
+        } else if (hourOfDay > 22 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
+
+
+

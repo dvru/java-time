ebd8b2877f9b4a465621b9b5106d0cd6d988f584
dvru <damvaru4@gmail.com>
2020-07-22 10:34:10 -0500

    keywords and expression

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args){
+        // a mile is equal to 1.609344 kilometers
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+        int score = 100; // score = 100 is an expression
+        if(score > 99) { // score > 99 is an expression
+            System.out.println("You got the high score!"); // "You got the high score!" is an expression
+            score = 0; // score = 0 is an expression
+        }
+
+
+    }
+
+}

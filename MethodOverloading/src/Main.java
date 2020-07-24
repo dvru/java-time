e7b13093f2e08d9f1230cf4e38c1c2c2a176b656
dvru <damvaru4@gmail.com>
2020-07-24 15:59:57 -0500

    Method Overloading

---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int newScore = calculateScore("Beth", 500);
+        System.out.println("New Score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score");
+        return 0;
+    }
+
+
+
+
+
+
+}

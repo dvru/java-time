8a75a4a19a243a723ca8288ddb8abd9272a666e3
dvru <damvaru4@gmail.com>
2020-07-24 18:41:12 -0500

    IntEqualityPrinter

--- challenge/src/IntEqualityPrinter.java
+++ challenge/src/IntEqualityPrinter.java
@@ -6,7 +6,6 @@ public class IntEqualityPrinter {
 
     }
 
-
     public static void printEqual(int num1, int num2, int num3) {
         if((num1 < 0) || (num2 < 0) || (num3 < 0)) {
             System.out.println("Invalid Value");

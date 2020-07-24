274ac2b369b6767f180dd0d380002557c59201ca
dvru <damvaru4@gmail.com>
2020-07-24 16:10:20 -0500

    Method Overloading

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("New Score is " + newScore);
         calculateScore(75);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6, -10);
 
     }
 
@@ -25,6 +26,21 @@ public class Main {
         return 0;
     }
 
+
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+
+        if((feet < 0 ) || ((inches < 0) || inches > 12)){
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+
+    }
 
 
 

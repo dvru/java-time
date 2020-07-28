20790a6bd026c7d505c7c0a6d49a5b80679edacc
dvru <damvaru4@gmail.com>
2020-07-28 13:14:02 -0500

    OOP

--- OOP/src/Car.java
+++ OOP/src/Car.java
@@ -1,5 +1,20 @@
 public class Car {
 
+    // these variables are the state of the car
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
 
+    //create a setter
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    //create a getter
+    public String getModel(){
+        return this.model;
+    }
 
 }
--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -2,6 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //create object based off Car class
+        Car porsche = new Car();
+        Car holden  = new Car();
+
+        System.out.println("Model is " + porsche.getModel()); // Model is null
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel()); // Model is Carrera
+
     }
 
 }

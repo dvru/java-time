08f2e93bf6b858140e3f3908d9d9a1b9318d6b97
dvru <damvaru4@gmail.com>
2020-08-04 13:40:46 -0500

    switch

--- ControlFlow/src/Switch.java
+++ ControlFlow/src/Switch.java
@@ -1,39 +1,37 @@
 public class Switch {
 
     public static void main(String [] args){
-//        int value = 3;
-//        if(value == 1){
-//            System.out.println("Value was 1");
-//        } else if(value == 2){
-//            System.out.println("Value was 2");
-//        } else {
-//            System.out.println("Was not 1 or 2");
-//        }
-
-//        int switchValue = 3;
-//
-//        switch (switchValue){
-//            case 1:
-//                System.out.println("Value was 1");
-//            break;
-//            case 2:
-//                System.out.println("Value was 2");
-//            break;
-//
-//            case 3: case 4: case 5:
-//                System.out.println("Was a 3, or a 4, or a 5");
-//                System.out.println("It was a " + switchValue);
-//                break;
-//
-//            default:
-//                System.out.println("Was not 1 or 2");
-//        }
 
 
+        int value = 3;
+        if(value == 1){
+            System.out.println("Value was 1");
+        } else if(value == 2){
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+
+
+        int switchValue = 3;
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+            break;
+            case 2:
+                System.out.println("Value was 2");
+            break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, or a 4, or a 5");
+                System.out.println("It was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+        }
 
 
         char charValue = 'A';
-
         switch(charValue){
             case 'A':case 'B':case 'C':case 'D':case 'E':
                 System.out.println("You found " + charValue);
@@ -43,6 +41,18 @@ public class Switch {
                 System.out.println("Was not A, B, C, D or E");
         }
 
+
+        String month = "January";
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("June");
+                break;
+            default:
+                System.out.println("Not Sure");
+        }
 
     }
 }

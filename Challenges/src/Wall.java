42d14a04eb1c052b59a559446124111adb93cedf
dvru <damvaru4@gmail.com>
2020-08-04 12:45:07 -0500

    WallArea

---
+++ Challenges/src/Wall.java
@@ -0,0 +1,85 @@
+
+public class Wall {
+
+    public static void main(String [] args){
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+
+
+
+
+    private double width;
+    private double height;
+
+    public Wall(){
+        this(0, 0);
+    }
+
+    public Wall(double width, double height) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        if( width < 0) {
+            this.width = 0;
+        } else{
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if( height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea(){
+       return width * height;
+    }
+}
+
+
+//    Write a class with the name Wall. The class needs two fields (instance variables) with name width and height of type double.
+//        The class needs to have two constructors. The first constructor does not have any parameters (no-arg constructor).
+//        The second constructor has parameters width and height of type double and it needs to initialize the fields. In case the width is less than 0 it needs to set the width field value to 0,
+//        in case the height parameter is less than 0 it needs to set the height field value to 0.
+
+//        Write the following methods (instance methods):
+//        Method named getWidth without any parameters, it needs to return the value of width field.
+//        Method named getHeight without any parameters, it needs to return the value of height field.
+
+//        Method named setWidth with one parameter of type double, it needs to set the value of the width field.
+//        If the parameter is less than 0 it needs to set the width field value to 0.
+
+//        Method named setHeight with one parameter of type double, it needs to set the value of the height field.
+//        If the parameter is less than 0 it needs to set the height field value to 0.
+//        Method named getArea without any parameters, it needs to return the area of the wall.
+
+
+
+

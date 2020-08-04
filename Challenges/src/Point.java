f79195b63a24ecd8db913c81e9d7066e3787913c
dvru <damvaru4@gmail.com>
2020-08-04 13:03:46 -0500

    Point

--- Challenges/src/Point.java
+++ Challenges/src/Point.java
@@ -55,3 +55,17 @@ public class Point {
         return distance(p.getX(), p.getY());
     }
 }
+
+//    You have to represent a point in 2D space. Write a class with the name Point. The class needs two fields (instance variables) with name x and y of type int.
+//        The class needs to have two constructors. The first constructor does not have any parameters (no-arg constructor).
+//        The second constructor has parameters x and y of type int and it needs to initialize the fields.
+//
+//        Write the following methods (instance methods):
+//        Method named getX without any parameters, it needs to return the value of x field.
+//        Method named getY without any parameters, it needs to return the value of y field.
+//        Method named setX with one parameter of type int, it needs to set the value of the x field.
+//        Method named setY with one parameter of type int, it needs to set the value of the y field.
+//        Method named distance without any parameters, it needs to return the distance between this Point and Point 0,0 as double.
+//        Method named distance with two parameters x, y both of type int, it needs to return the distance between this Point and Point x,y as double.
+//        Method named distance with parameter another of type Point, it needs to return the distance between this Point and another Point as double.
+

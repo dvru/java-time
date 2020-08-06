e013059d6b06e3a06439b40ab4618604a8e9442a
dvru <damvaru4@gmail.com>
2020-08-06 18:19:10 -0500

    Reading User input

--- ControlFlow/src/ReadUserInput.java
+++ ControlFlow/src/ReadUserInput.java
@@ -6,16 +6,25 @@ public class ReadUserInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2020 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
 
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2020 - yearOfBirth;
+
+            if(age >= 0 && age <= 100){
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to pass year of birth");
+        }
         scanner.close();
-
     }
 }

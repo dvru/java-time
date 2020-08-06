c81e6081a3e39562a21bdac6ec907e3dffce07ee
dvru <damvaru4@gmail.com>
2020-08-06 13:27:26 -0500

    while and do while

--- ControlFlow/src/While.java
+++ ControlFlow/src/While.java
@@ -1,23 +1,56 @@
 public class While {
 
     public static void main(String [] args) {
-        int count = 1;
-        while(count != 6){
-            System.out.println("Count value is "+ count);
+        int count = 6;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
             count++;
         }
 
 //        for(count = 1; count != 6; count++){
 //            System.out.println("Count value is "+ count);
 //        }
+//        count = 1;
+//        while(true) {
+//            if(count == 5){
+//                break;
+//            }
+//            System.out.println("Count value is "+ count);
+//            count++;
+
+
         count = 1;
-        while(true) {
-            if(count == 5){
+        do {
+            System.out.println("Count value is " + count);
+            count++;
+        } while (count != 6);
+
+
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumbersFound = 0;
+
+        while (number <= finishNumber){
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number " + number);
+            evenNumbersFound++;
+            if(evenNumbersFound >= 5){
                 break;
             }
-            System.out.println("Count value is "+ count);
-            count++;
         }
+        System.out.println("Total even numbers found " + evenNumbersFound);
     }
 
+
+    public static boolean isEvenNumber(int number) {
+        if((number % 2) == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }

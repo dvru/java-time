82eb5d6ff4f3081bad059d878fd3fe99cf1d2eb7
dvru <damvaru4@gmail.com>
2020-08-06 21:06:49 -0500

    Reading User input

---
+++ ControlFlow/src/PaintJob.java
@@ -0,0 +1,24 @@
+class PaintJob {
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int availableBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || availableBucket < 0) {
+            return -1;
+        }
+
+        double totalWallArea = width * height;
+        double additionalAreaToCover = totalWallArea - (areaPerBucket*availableBucket);
+
+        return (int) Math.ceil(additionalAreaToCover/areaPerBucket);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        return getBucketCount(width,height,areaPerBucket,0);
+    }
+
+    public static int getBucketCount(double areaToCover, double areaPerBucket) {
+        if(areaPerBucket <= 0 || areaToCover <= 0) {
+            return -1;
+        }
+        return (int) Math.ceil(areaToCover/areaPerBucket);
+    }
+}

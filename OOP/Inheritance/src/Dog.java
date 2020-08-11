39267bac9b9b84570ef904ed95ed359ce74cdec4
dvru <damvaru4@gmail.com>
2020-08-11 14:08:27 -0500

    Inheritance

---
+++ OOP/Inheritance/src/Dog.java
@@ -0,0 +1,30 @@
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    //constructor inherits from Animal class, super calls the Constructor you're inheriting from.
+    public Dog(String name,  int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew(){
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+}

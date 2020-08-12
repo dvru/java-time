a01f804b542ab15add1461a5e0866ca5783376dc
dvru <damvaru4@gmail.com>
2020-08-12 17:27:02 -0500

    Cylinder

---
+++ OOP/Inheritance/src/Circle.java
@@ -0,0 +1,29 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius){
+        if(radius < 0){
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+}
+
+//Write a class with the name Circle. The class needs one field (instance variable) with name radius of type double.
+//        The class needs to have one constructor with parameter radius of type double and it needs to initialize the fields.
+//        In case the radius parameter is less than 0 it needs to set the radius field value to 0.
+//        Write the following methods (instance methods):
+//        Method named getRadius without any parameters, it needs to return the value of radius field.
+//        Method named getArea without any parameters, it needs to return the calculated area (radius * radius * PI).
+//        For PI use Math.PI constant.

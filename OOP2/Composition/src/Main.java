0ca162a12456e546822c4e07f7df68f7c8bce3ae
dvru <damvaru4@gmail.com>
2020-09-08 11:13:55 -0500

    Inheritance

--- OOP2/Composition/src/Main.java
+++ OOP2/Composition/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
-    public static void main(String[] arg){
+    public static void main(String[] args) {
         Dimensions dimensions = new Dimensions(20, 20, 5);
+
         Case theCase = new Case("220B", "Dell", "240", dimensions);
 
         Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
@@ -21,21 +22,21 @@ public class Main {
         // but to access the object used in composition within the main class
         // like you saw in this video.
 
-        Wall wall1 = new Wall("West");
-        Wall wall2 = new Wall("East");
-        Wall wall3 = new Wall("South");
-        Wall wall4 = new Wall("North");
-
-        Ceiling ceiling = new Ceiling(12, 55);
-
-        Bed bed = new Bed("Modern", 4, 3, 2, 1);
-
-        Lamp lamp = new Lamp("Classic", false, 75);
-
-        Bedroom bedRoom = new Bedroom("D", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
-        bedRoom.makeBed();
-
-        bedRoom.getLamp().turnOn();
+//        Wall wall1 = new Wall("West");
+//        Wall wall2 = new Wall("East");
+//        Wall wall3 = new Wall("South");
+//        Wall wall4 = new Wall("North");
+//
+//        Ceiling ceiling = new Ceiling(12, 55);
+//
+//        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+//
+//        Lamp lamp = new Lamp("Classic", false, 75);
+//
+//        Bedroom bedRoom = new Bedroom("D", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+//        bedRoom.makeBed();
+//
+//        bedRoom.getLamp().turnOn();
 
 
     }

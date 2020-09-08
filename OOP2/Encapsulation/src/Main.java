e7e24df8446fdce0b2aa3f4e316e87e6230cac2c
dvru <damvaru4@gmail.com>
2020-09-08 12:44:49 -0500

    Composition

---
+++ OOP2/Encapsulation/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args){
+//        Player player = new Player();
+//        player.fullName = "Kim Hu";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println(player.fullName + " has a remaining health of = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println(player.fullName + " has a remaining health of = " + player.healthRemaining());
+
+
+
+        EnhancedPlayer player = new EnhancedPlayer("Beth", 200, "Sword");
+        System.out.println("Initial Health is " + player.getHealth());
+    }
+}

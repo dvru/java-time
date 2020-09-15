0b88e7b56d3cb1db1fc275872c4d9528e811e08a
dvru <damvaru4@gmail.com>
2020-09-15 11:15:04 -0500

    OOP2

---
+++ OOP2/Encapsulation/src/Holden.java
@@ -0,0 +1,38 @@
+class Holden extends Car {
+
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Holden -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Holden -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Holden -> brake()";
+    }
+}
+
+
+// We are going to go back to the car analogy.
+// Create a base class called Car
+// It should have a few fields that would be appropriate for a generice car calss.
+// engine, cylinders, wheels, etc.
+// Constructor should initialize cylinders (number of) and name, and set wheels to 4
+// and engine to true. Cylinders and names would be passed parameters.
+//
+// Create appropriate getters
+//
+// Create some methods like startEngine, accelerate, and brake
+//
+// show a message for each in the base class
+// Now create 3 sub classes for your favorite vehicles.
+// Override the appropriate methods to demonstrate polymorphism in use.
+// put all classes in the one java file (this one).

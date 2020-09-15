6f03fb0b36c7c20cb348cecd6b6cb82e85e44e25
dvru <damvaru4@gmail.com>
2020-09-15 09:54:57 -0500

    OOP2

--- OOP2/Encapsulation/src/Main.java
+++ OOP2/Encapsulation/src/Main.java
@@ -14,15 +14,26 @@ public class Main {
 //        player.health = 200;
 //        player.loseHealth(damage);
 //        System.out.println(player.fullName + " has a remaining health of = " + player.healthRemaining());
-
         EnhancedPlayer player = new EnhancedPlayer("Beth", 200, "Sword");
         System.out.println("Initial Health is " + player.getHealth());
 
 
+
+        // Create a class and demonstrate proper encapsulation techniques
+        // the class will be called Printer
+        // It will simulate a real Computer Printer
+        // It should have fields for the toner Level, number of pages printed, and
+        // also whether its a duplex printer (capable of printing on both sides of the paper).
+        // Add methods to fill up the toner (up to a maximum of 100%), another method to
+        // simulate printing a page (which should increase the number of pages printed).
+        // Decide on the scope, whether to use constructors, and anything else you think is needed.
+
         Printer printer = new Printer(50, false);
         System.out.println("initial page count = " +printer.getPagesPrinted());
+
         int pagesPrinted = printer.printPages(4);
         System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
+
         pagesPrinted = printer.printPages(2);
         System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
 

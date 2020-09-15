6c406a6992b334c74ecade2ad9720ca7b53d77de
dvru <damvaru4@gmail.com>
2020-09-15 10:28:22 -0500

    OOP2

--- OOP2/Encapsulation/src/Main2.java
+++ OOP2/Encapsulation/src/Main2.java
@@ -9,6 +9,10 @@ class Movie {
     public String plot() {
         return "No Plot here";
     }
+
+    public String getName() {
+        return name;
+    }
 }
 
 class Jaws extends Movie {
@@ -55,6 +59,8 @@ class StarWars extends Movie{
     }
 }
 
+
+
 class Forgettable extends Movie{
     public Forgettable() {
         super("Forgettable");
@@ -64,12 +70,34 @@ class Forgettable extends Movie{
 
 
 
-
-
 public class Main2 {
 
     public static void main(String [] args) {
+        for (int i=1; i < 11; i++){
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i +
+                                " : " + movie.getName() + "\n" +
+                                "Plot: " + movie.plot() + "\n");
+        }
 
     }
 
+    public static Movie randomMovie(){
+        int randomNumber = (int) (Math.random() * 5) +1;
+        System.out.println("Random Number generated: " + randomNumber);
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgettable();
+        }
+
+        return null;
+    }
 }

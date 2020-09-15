6a2e5596eed5a259b077ea1545417810245a0c02
dvru <damvaru4@gmail.com>
2020-09-15 11:13:11 -0500

    OOP2

---
+++ OOP2/Encapsulation/src/Main3.java
@@ -0,0 +1,111 @@
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubishi extends Car {
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+
+
+
+public class Main3 {
+    public static void main(String[] args){
+
+        Car car = new Car(8, "Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Car ford = new Car(6, "Ford");
+//        { // inline class
+//            @Override
+//            public String startEngine() {
+//                return "Ford -> startEngine()";
+//            }
+//
+//            @Override
+//            public String accelerate() {
+//                return "Ford -> accelerate()";
+//            }
+//
+//            @Override
+//            public String brake() {
+//                return "Ford -> brake()";
+//            }
+//        };
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+    }
+}
+
+// We are going to go back to the car analogy.
+// Create a base class called Car
+// It should have a few fields that would be appropriate for a generice car calss.
+// engine, cylinders, wheels, etc.
+// Constructor should initialize cylinders (number of) and name, and set wheels to 4
+// and engine to true. Cylinders and names would be passed parameters.
+//
+// Create appropriate getters
+//
+// Create some methods like startEngine, accelerate, and brake
+//
+// show a message for each in the base class
+// Now create 3 sub classes for your favorite vehicles.
+// Override the appropriate methods to demonstrate polymorphism in use.
+// put all classes in the one java file (this one).

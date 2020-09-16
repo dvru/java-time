7da3f02569d62bea870b81a65903ec3574d68806
dvru <damvaru4@gmail.com>
2020-09-16 11:08:46 -0500

    Arrays

---
+++ Arrays/src/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+
+    public static void main(String [] args){
+
+//      int[] myIntArray;
+//      myIntArray = new int[10];
+        int[] myIntArray = new int[10]; // define array and assign a new array to a variable
+        myIntArray[5] = 50; // element 5 is 6 within an array
+
+        double[] myDoubleArray = new double[10];
+        System.out.println(myIntArray[5]);
+    }
+
+
+
+}

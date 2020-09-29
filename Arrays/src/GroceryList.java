64a528a6327e9f3f31752865c1c0535b528820c0
dvru <damvaru4@gmail.com>
2020-09-29 11:10:09 -0500

    GroceryList

---
+++ Arrays/src/GroceryList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item){ //add an element
+        groceryList.add(item);
+    }
+
+    //size will return many elements I've currently stored in the array list
+
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i = 0; i<groceryList.size(); i++){
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+
+    }
+}

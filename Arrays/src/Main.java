fad0f58fb366adffb374d57df113a4b1be72b7f3
dvru <damvaru4@gmail.com>
2020-09-29 11:40:20 -0500

    GroceryList

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-
     private static Scanner scanner = new Scanner(System.in);
     private static GroceryList groceryList = new GroceryList();
 
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         boolean quit = false;
         int choice = 0;
         printInstructions();

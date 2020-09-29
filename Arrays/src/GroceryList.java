b1f42bebc332729e8741d85faef28bef48bebc08
dvru <damvaru4@gmail.com>
2020-09-29 11:16:41 -0500

    GroceryList

--- Arrays/src/GroceryList.java
+++ Arrays/src/GroceryList.java
@@ -24,6 +24,15 @@ public class GroceryList {
     public void removeGroceryItem(int position){
         String theItem = groceryList.get(position);
         groceryList.remove(position);
+    }
 
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+
+        int position = groceryList.indexOf(searchItem);
+        if(position >=0){
+            return groceryList.get(position);
+        }
+        return null;
     }
 }
